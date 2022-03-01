1f6a2479502065cac2d10f71aeffba58d6d64f35
ThunderKlappe <timschley@gmail.com>
2022-03-01 13:32:01 -0800

    form validates all info and changes css. Displays info on console if valid

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 import "./style.css";
 
 const formValidation = (() => {
+    const _markInvalid = elem => {
+        elem.classList.remove("valid");
+        elem.classList.add("invalid");
+    };
+    const _markValid = elem => {
+        elem.classList.remove("invalid");
+        elem.classList.add("valid");
+    };
+
     const checkName = () => {
         let name = document.querySelector("#name-input");
         name.setCustomValidity("");
         if (name.value == "") {
             name.setCustomValidity("Please enter your name");
             name.reportValidity();
+            _markInvalid(name);
+            return false;
         }
+        _markValid(name);
+        return true;
     };
     const checkEmail = () => {
         let email = document.querySelector("#email-input");
@@ -17,7 +30,11 @@ const formValidation = (() => {
         if (!emRegex.test(email.value)) {
             email.setCustomValidity("Please enter a valid email address");
             email.reportValidity();
+            _markInvalid(email);
+            return false;
         }
+        _markValid(email);
+        return true;
     };
     const checkCountry = () => {
         let country = document.querySelector("#country-input");
@@ -25,7 +42,11 @@ const formValidation = (() => {
         if (country.value == "") {
             country.setCustomValidity("Please enter your country");
             country.reportValidity();
+            _markInvalid(country);
+            return false;
         }
+        _markValid(country);
+        return true;
     };
     const checkZip = () => {
         let zip = document.querySelector("#zip-input");
@@ -34,7 +55,11 @@ const formValidation = (() => {
         if (!zipRegex.test(zip.value)) {
             zip.setCustomValidity("Please enter a valid zip code (5 digits or 5-4 digits)");
             zip.reportValidity();
+            _markInvalid(zip);
+            return false;
         }
+        _markValid(zip);
+        return true;
     };
     const checkPass = () => {
         let pass = document.querySelector("#password-input");
@@ -43,7 +68,11 @@ const formValidation = (() => {
         if (!passRegex.test(pass.value)) {
             pass.setCustomValidity("Please enter a valid password");
             pass.reportValidity();
+            _markInvalid(pass);
+            return false;
         }
+        _markValid(pass);
+        return true;
     };
     const checkPassConf = () => {
         let passConf = document.querySelector("#password-conf-input");
@@ -52,10 +81,21 @@ const formValidation = (() => {
         if (passConf.value != pass.value) {
             passConf.setCustomValidity("Please re-enter your password");
             passConf.reportValidity();
+            _markInvalid(passConf);
+            return false;
+        }
+        _markValid(passConf);
+        return true;
+    };
+
+    const submitForm = e => {
+        e.preventDefault();
+        if (checkName() && checkEmail() && checkCountry() && checkZip() && checkPass() && checkPassConf()) {
+            document.querySelectorAll("input").forEach(input => console.log(input.value + ", "));
         }
     };
 
-    return { checkName, checkEmail, checkCountry, checkZip, checkPass, checkPassConf };
+    return { checkName, checkEmail, checkCountry, checkZip, checkPass, checkPassConf, submitForm };
 })();
 
 // eslint-disable-next-line no-unused-vars
@@ -66,6 +106,8 @@ const initListeners = (() => {
     document.querySelector("#zip-input").addEventListener("change", formValidation.checkZip);
     document.querySelector("#password-input").addEventListener("change", formValidation.checkPass);
     document.querySelector("#password-conf-input").addEventListener("change", formValidation.checkPassConf);
+
+    document.querySelector("#submit-button").addEventListener("click", formValidation.submitForm);
 
     // document.querySelector("#name-input").addEventListener("input", formValidation.checkName);
     // document.querySelector("#email-input").addEventListener("input", formValidation.checkEmail);
