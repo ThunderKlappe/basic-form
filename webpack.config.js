6990de6228fb1fade9661e5a8bec73b6b6ea03e2
ThunderKlappe <timschley@gmail.com>
2022-02-28 18:06:03 -0800

    Project set up

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-undef */
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+
+
+module.exports = {
+    mode: "production",
+    entry: {
+        index: "./src/index.js",
+    },
+    //devtool: "inline-source-map",
+    plugins: [
+        new HtmlWebpackPlugin({
+        title: "Basic Sign-up Form",
+        template: "src/index.html"
+    }),
+    ],
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "dist"),
+        clean:true,
+    },
+    module: {
+        rules: [
+        {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+        },
+        ],
+    },
+};
